f6661da73ed630d674b538c450dc1510c6d64bd2
Sowmya0204 <loyasowmya2000@gmail.com>
2025-10-29 17:58:01 +0530

    Remove React import and update type imports

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,17 +1,2 @@
-import React from 'react';
-import { Book } from '../types.ts';
-import { BookCard } from './BookCard.tsx';
-
-interface BookListProps {
-  books: Book[];
-}
-
-export const BookList: React.FC<BookListProps> = ({ books }) => {
-  return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
-      {books.map((book) => (
-        <BookCard key={book.key} book={book} />
-      ))}
-    </div>
-  );
-};
+import { Book } from '../types';
+import { BookCard } from './BookCard';
